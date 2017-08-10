c6cbd63c0467bc4a2bcabcb16a4bf567263e5b0a
Artem Solovev <artem.solovev.lse@gmail.com>
2017-08-10 16:55:05 +0300

    Added script that paints a stairway

---
+++ 01_week/stairways.py
@@ -0,0 +1,14 @@
+import sys
+
+user_input = int( sys.argv[1] )
+symbols = ""
+spaces = ""
+
+for space in range( user_input ):
+    spaces += " "
+
+for i in range( user_input ):
+    symbols += "*"
+    spaces = spaces[:-1]
+
+    print( spaces + symbols + "\n" )
