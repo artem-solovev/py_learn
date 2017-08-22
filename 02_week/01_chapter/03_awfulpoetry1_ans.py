613445c33ca1e72571074668879a32d4ce4c04c7
Artem Solovev <artem.solovev.lse@gmail.com>
2017-08-22 19:22:59 +0300

    Added script that makes beautiful poetry

---
+++ 02_week/01_chapter/03_awfulpoetry1_ans.py
@@ -0,0 +1,34 @@
+import random
+
+print( "__WELCOME TO VOGON POETRY__" )
+print( "___________________________" )
+
+articles = [ 'a', 'the', 'an' ]
+
+nouns = ['time', 'person', 'year', 'way', 'day', 'thing', 'man',
+'world', 'life', 'hand', 'part', 'child', 'eye', 'woman',
+'place', 'work', 'week', 'case', 'point', 'government',
+'company', 'number', 'group', 'problem', 'fact']
+
+verbs = ['be', 'have', 'do', 'say', 'get', 'make', 'go',
+'know', 'take', 'see', 'come', 'think', 'look',
+'want', 'give', 'use', 'find', 'tell', 'ask',
+'work', 'seem', 'feel', 'try', 'leave', 'call']
+
+adverbs = ['quickly', 'cheerfully', 'painfully',
+'easily', 'secretly', 'quietly','peacefully', 'cleverly',
+'often', 'sometimes', 'every week', 'monthly', 'always',
+'never', 'hourly', 'seldom', 'frequently', 'annually']
+
+strSequence = [ articles, nouns, adverbs, verbs ]
+
+userChoise = int( input( "Enter the number: " ) )
+
+
+for i in range( 0, userChoise ):
+    randStr = ""
+
+    for j in range( len( strSequence ) ):
+        randStr += strSequence[j][ random.randint( 0, len( strSequence[j] ) - 1 ) ] + " "
+
+    print( randStr )
